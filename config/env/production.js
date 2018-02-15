52dcb18b7ab47a3a382df4a6329b3515d31bbd7b
Courtney Jooste <courtney@codelab.io>
2018-02-15 12:45:57 +0200

    fix: i was wrong

--- config/env/production.js
+++ config/env/production.js
@@ -13,8 +13,8 @@
 module.exports = {
 
   ssl: {
-    key: require('fs').readFileSync('/home/ubuntu/projects/erinvale-api/certificates/privkey.pem'),
-    cert: require('fs').readFileSync('/home/ubuntu/projects/erinvale-api/certificates/fullchain.pem'),
+    key: require('fs').readFileSync('/etc/certificates/privkey.pem'),
+    cert: require('fs').readFileSync('/etc/certificates/fullchain.pem'),
     // ca: require('fs').readFileSync('/etc/letsencrypt/live/api.scanplan.co.za/chain.pem')
   },
 
